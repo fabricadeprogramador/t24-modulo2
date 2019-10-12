5cd7470f94019a12b9e73c12ec4b9fb963e95898
Alison Moura <alison.oghino@gmail.com>
2019-10-12 08:57:21 -0400

    Adicionando projeto clientela

---
+++ clientela/src/components/FormClient.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-container>
+        <v-form>
+            <v-row>
+                <v-col>
+                    <v-text-field outlined label="Nome do cliente"></v-text-field>
+                </v-col>
+                <v-col>
+                    <v-text-field outlined label="Email do cliente"></v-text-field>
+                </v-col>
+                <v-col>
+                    <v-text-field outlined type="number" label="CPF do cliente"></v-text-field>
+                </v-col>
+                <v-col>
+                    <v-text-field outlined type="number" label="CEP do cliente"></v-text-field>
+                </v-col>
+            </v-row>
+            <v-row class="px-3">
+                <v-btn text color="error">Cancelar</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn color="success">Salvar</v-btn>
+            </v-row>
+        </v-form>
+    </v-container>
+</template>
---
+++ clientela/src/store.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    usuarioLogado: {
+      nome: "Jão da Silva",
+      email: "jao@gmail.com"
+    }
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  }
+})
